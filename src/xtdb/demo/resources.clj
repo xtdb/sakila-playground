3930231e18a6576ddf18bdf06ec5b50eef1dbd93
Malcolm Sparks <mal@juxt.pro>
2023-12-27 11:44:48 +0000

    Add some basic resources

---
+++ src/xtdb/demo/resources.clj
@@ -0,0 +1,30 @@
+;; Copyright © 2023, JUXT LTD.
+
+(ns xtdb.demo.resources
+  {:web-context "/"}
+  (:require
+   [xtdb.demo.web.resource :refer [map->Resource html-resource]]
+   [xtdb.demo.web.html :as html]
+   [xtdb.demo.db :refer [xt-node]]
+   [hiccup2.core :as h]
+   [xtdb.api :as xt]))
+
+(def hello
+  (let [state (atom {:greeting "Hello"})]
+    (map->Resource
+     {:representations
+      [^{:headers {"content-type" "text/html;charset=utf-8"}}
+       (fn [] (format "<h1>%s World!</h1>\r\n" (:greeting @state)))
+       ]})))
+
+(def select-films
+  "select film.title, film.description from film order by film.title")
+
+(def films
+  (html-resource
+   (fn []
+     (->
+      (xt/q xt-node select-films)
+      (html/html-table {:rowspecs [:title :description]})
+      (h/html)
+      (str "\r\n")))))
