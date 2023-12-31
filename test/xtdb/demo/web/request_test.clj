77f1cfae6658bcd6ef4ebeef30818a3034491dd9
Malcolm Sparks <mal@juxt.pro>
2023-12-31 17:00:23 +0000

    Add request header and body parsing

--- test/xtdb/demo/web/request_test.clj
+++ test/xtdb/demo/web/request_test.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [xtdb.demo.web.request :as req]
+   [xtdb.demo.web.reap :refer [make-header-reader]]
    [xtdb.demo.web.resource :refer [map->Resource]]))
 
 (defn test-resource []
@@ -9,13 +10,19 @@
    {
     :methods
     {"POST"
-     {:handler (fn [req] (throw (ex-info "TODO" {})))
+     {:accept ["application/x-www-form-urlencoded"]
       :max-content-length 100000
-      }}
+      :handler (fn [req] (throw (ex-info "TODO" {})))}}}))
 
-    }))
-
-
+(defn test-request
+  ([method headers body-str]
+   (let [req {:ring.request/method method
+              :ring.request/headers headers}]
+     (cond-> req
+       body-str (assoc :ring.request/body (java.io.ByteArrayInputStream. (.getBytes body-str)))
+       true (assoc :header-reader (make-header-reader headers)))))
+  ([method headers]
+   (test-request method headers nil)))
 
 (deftest request-handling-test
   (let [resource (test-resource)]
@@ -24,9 +31,7 @@
       (is
        (thrown-with-msg?
         clojure.lang.ExceptionInfo
-
         #"No Content-Length header found"
-
         (req/receive-representation
          resource
          {:ring.request/method :post}))))
@@ -35,13 +40,10 @@
       (is
        (thrown-with-msg?
         clojure.lang.ExceptionInfo
-
         #"Bad content length"
-
         (req/receive-representation
          resource
-         {:ring.request/method :post
-          :ring.request/headers {"content-length" "ABC"}}))))
+         (test-request :post {"content-length" "ABC"})))))
 
     (testing "Max content length exceeded"
       (is
@@ -49,11 +51,8 @@
         clojure.lang.ExceptionInfo
         #"Payload too large"
         (req/receive-representation
-         (test-resource
-          )
-         {:ring.request/method :post
-          :ring.request/headers {"content-length" "120000"}})))
-      )
+         (test-resource)
+         (test-request :post {"content-length" "120000"})))))
 
     (testing "No body in request"
       (is
@@ -62,5 +61,30 @@
         #"No body in request"
         (req/receive-representation
          (test-resource)
-         {:ring.request/method :post
-          :ring.request/headers {"content-length" "10"}}))))))
+         (test-request :post {"content-length" "10"})))))
+
+    (testing "Malformed content-type"
+      (is
+       (thrown-with-msg?
+        clojure.lang.ExceptionInfo
+        #"Malformed content-type"
+        (req/receive-representation
+         (test-resource)
+         (test-request
+          :post
+          {"content-length" "10"
+           "content-type" "application"}
+          "foo=bar")))))
+
+    (testing "application/x-www-form-urlencoded decoded"
+      (let [body "a=b&c=d"]
+        (is
+         (=
+          {"a" "b", "c" "d"}
+          (req/read-request-body
+           (test-resource)
+           (test-request
+            :post
+            {"content-length" (str (count (.getBytes body)))
+             "content-type" "application/x-www-form-urlencoded"}
+            body))))))))
