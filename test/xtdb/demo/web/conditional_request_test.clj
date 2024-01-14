a47a26c42463790cc2035611bdcbede77094ef69
Malcolm Sparks <mal@juxt.pro>
2024-01-14 19:59:19 +0000

    Rename test

--- test/xtdb/demo/web/conditional_request_test.clj
+++ test/xtdb/demo/web/conditional_request_test.clj
@@ -1,9 +1,12 @@
-(ns xtdb.demo.web.last-modified-test
+(ns xtdb.demo.web.conditional-request-test
   (:require
    [clojure.test :refer [deftest is testing]]
    [xtdb.demo.web.methods :as methods]
    [xtdb.demo.web.resource :refer [map->Resource]]
-   [juxt.reap.encoders :refer [format-http-date]]))
+   [juxt.reap.encoders :refer [format-http-date]])
+  (:import
+   (java.time Instant)
+   (java.util Date)))
 
 (deftest last-modified-test
   (is
@@ -23,3 +26,20 @@
                 :ring.response/headers {"last-modified" (format-http-date (java.util.Date/from (java.time.Instant/parse "2024-01-01T10:00:00Z")))}
                 :ring.response/body "foo"})]})]
       (methods/GET resource {:ring.request/method :get})))))
+
+
+(let [request {:ring.request/method :get}
+      resource
+      (fn [req]
+        (map->Resource
+         {:representations
+          [^{"content-type" "text/plain"
+             "last-modified"
+             (-> "2024-01-01T10:00:00Z"
+                 Instant/parse
+                 Date/from
+                 format-http-date)}
+           (fn [_]
+             {:ring.response/body "foo"})]}))]
+
+  (methods/GET (resource request) request))
