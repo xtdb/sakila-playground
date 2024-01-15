351926ad6ef745d23d4d035690eec3ebc92076e3
Yuriy Zaytsev <yurkazaytsev@gmail.com>
2024-01-15 16:44:56 +0000

    top rented films

--- dev/yry/repl.clj
+++ dev/yry/repl.clj
@@ -11,3 +11,18 @@
   
   (tap> 1)
   )
+
+(comment 
+  "Query helplers"
+  
+  (require '[xtdb.api :as xt])
+  
+  (defn tap>query
+    [xt-node query & args]
+    (-> xt-node
+        :xt-node
+        (xt/q query args)
+        (with-meta {:portal.viewer/default :portal.viewer/table})
+        tap>))
+  
+  )
