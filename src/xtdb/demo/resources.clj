0ab7e79900c91c15e20d1992e6f9685637acfa24
Malcolm Sparks <mal@juxt.pro>
2024-01-16 14:58:31 +0000

    Wrap in customer detail response as ring.response/body

--- src/xtdb/demo/resources.clj
+++ src/xtdb/demo/resources.clj
@@ -214,15 +214,16 @@
   (html-resource
    (fn [_]
      (let [customer (first (xt/q (:xt-node xt-node) (format "SELECT customer.xt$id AS id, customer.first_name, customer.last_name, customer.email, address.phone FROM customer, address WHERE customer.xt$id = %s AND customer.address_id = address.xt$id" (get path-params "id"))))]
-       (str (h/html [:dl
-                     [:dt "First name"]
-                     [:dd (:first_name customer)]
-                     [:dt "Last name"]
-                     [:dd (:last_name customer)]
-                     [:dt "Email"]
-                     [:dd (:email customer)]
-                     [:dt "Phone"]
-                     [:dd (:phone customer)]]))))))
+       {:ring.response/body
+        (str (h/html [:dl
+                      [:dt "First name"]
+                      [:dd (:first_name customer)]
+                      [:dt "Last name"]
+                      [:dd (:last_name customer)]
+                      [:dt "Email"]
+                      [:dd (:email customer)]
+                      [:dt "Phone"]
+                      [:dd (:phone customer)]]))}))))
 
 (comment
   (take 10 (xt/q (:xt-node xt-node) "select customer.xt$id as id, customer.* from customer")))
