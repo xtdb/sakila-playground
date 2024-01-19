c186ac5ebcbc2627791d00a9c8e40491b9e56bd2
Yuriy Zaytsev <yurkazaytsev@gmail.com>
2024-01-19 13:50:43 +0000

    top renting customers

---
+++ resources/sql/top-renting-customers.sql
@@ -0,0 +1,14 @@
+WITH top_user AS 
+    (SELECT rental.customer_id, 
+            count(*) AS films_rented
+    FROM rental
+    GROUP BY rental.customer_id
+    ORDER BY films_rented DESC
+    LIMIT 10)
+SELECT top_user.customer_id, 
+        top_user.films_rented,
+        customer.last_name,
+        customer.first_name,
+        customer.email            
+FROM top_user
+LEFT JOIN customer ON top_user.customer_id = customer.xt$id
