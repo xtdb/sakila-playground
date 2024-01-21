da680b29a7fbe9bda4a1bb8a6daeacf88298ab31
Malcolm Sparks <mal@juxt.pro>
2024-01-21 16:58:04 +0000

    Show SQL in details element

--- resources/sql/customer-historic-rentals.sql
+++ resources/sql/customer-historic-rentals.sql
@@ -1,4 +1,8 @@
-SELECT rental.xt$id as id, film.title, rental.rental_id, rental.customer_id, rental.inventory_id, rental.xt$valid_from as rental_date, rental.xt$valid_to as return_date
+SELECT rental.xt$id as id,
+       film.title,
+       rental.rental_id, rental.customer_id, rental.inventory_id,
+       rental.xt$valid_from as rental_date,
+       rental.xt$valid_to as return_date
 FROM rental FOR ALL VALID_TIME
 LEFT JOIN customer ON (rental.customer_id = rental.xt$id)
 LEFT JOIN inventory ON (rental.inventory_id = inventory.xt$id)
