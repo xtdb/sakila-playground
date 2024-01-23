4ed1556a3b06d80217668bee508dfd32cafcf85c
Dan Stone <wotbrew@gmail.com>
2024-01-23 13:17:15 +0000

    Oops remove comment

--- sql/queries/available_films.sql
+++ sql/queries/available_films.sql
@@ -8,7 +8,6 @@ SELECT
 FROM store s
 JOIN inventory i ON i.store_id = s.xt$id
 JOIN film f ON f.xt$id = i.film_id
--- NOT EXISTS seems very slow...
 LEFT JOIN rental r ON r.inventory_id = i.xt$id
 WHERE r.xt$id IS NULL
 GROUP BY s.xt$id, f.title
