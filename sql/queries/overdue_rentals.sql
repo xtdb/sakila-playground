36a6f396d527dd9466faf78c1ca5035b655e9394
Dan Stone <wotbrew@gmail.com>
2024-01-23 11:10:46 +0000

    Add port of overdue films query

---
+++ sql/queries/overdue_rentals.sql
@@ -0,0 +1,16 @@
+-- :desc "Shows overdue films, using the xt$valid_from as the rental date"
+SELECT
+  customer.xt$id customer_id,
+  customer.first_name,
+  customer.last_name,
+  address.phone,
+  film.xt$id film_id,
+  film.title,
+  rental.xt$valid_from rental_date
+FROM rental
+JOIN customer ON rental.customer_id = customer.xt$id
+JOIN address ON customer.address_id = address.xt$id
+JOIN inventory ON rental.inventory_id = inventory.xt$id
+JOIN film ON inventory.film_id = film.xt$id
+WHERE rental.xt$valid_from + film.rental_duration DAY < CURRENT_DATE
+ORDER BY rental_date
