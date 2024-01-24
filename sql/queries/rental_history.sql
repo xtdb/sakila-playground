b37043660a82afc2e4acb9d64ca4a2422e149f26
Dan Stone <wotbrew@gmail.com>
2024-01-24 13:24:34 +0000

    Add rental history

---
+++ sql/queries/rental_history.sql
@@ -0,0 +1,18 @@
+-- :category "Custom"
+-- :params {:id :long}
+-- :defaults {:id 42}
+-- :param-order [:id]
+-- :desc "History of a particular tape"
+-- :col-order [:title :last_name :first_name, :rental_date, :return_date]
+SELECT
+  f.title,
+  c.last_name,
+  c.first_name,
+  r.xt$valid_from rental_date,
+  r.xt$valid_to return_date
+FROM inventory i
+JOIN rental FOR ALL VALID_TIME r ON r.inventory_id = i.xt$id
+JOIN film f ON f.xt$id = i.film_id
+JOIN customer c ON c.xt$id = r.customer_id
+WHERE i.xt$id = ?
+ORDER BY return_date DESC
