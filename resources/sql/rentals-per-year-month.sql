4780115c808081a36756c2775f1213ab102379e2
Yuriy Zaytsev <yurkazaytsev@gmail.com>
2024-01-26 18:05:03 +0000

    Add some charts

--- resources/sql/rentals-per-year-month.sql
+++ resources/sql/rentals-per-year-month.sql
@@ -1,9 +1,8 @@
 WITH rentals_ym AS (
     SELECT EXTRACT (YEAR FROM rental.xt$valid_from) as year,
-           EXTRACT (MONTH FROM rental.xt$valid_from) as month,
-           rental.xt$valid_from as ts
+           EXTRACT (MONTH FROM rental.xt$valid_from) as month
     FROM rental)
-SELECT A.year, A.month, count(*) as rented, max(A.ts) as ts
+SELECT A.year, A.month, count(*) as rented
 FROM rentals_ym as A
 GROUP BY A.year, A.month
 ORDER BY A.year DESC, A.month DESC
