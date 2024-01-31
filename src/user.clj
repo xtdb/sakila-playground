d492f612e6baae64d6074c25985249d586d2ac99
James Henderson <james@jarohen.dev>
2024-01-31 14:55:06 +0000

    migrate to latest snapshot

--- src/user.clj
+++ src/user.clj
@@ -16,24 +16,13 @@
     (with-open [rdr (io/reader file)]
       (doseq [line-batch (->> (line-seq rdr)
                              (partition-all 1000))]
-        (xt/submit-tx node
-          (for [line line-batch]
-            (let [data (edn/read-string {:readers {'time/instant #(Instant/parse %)}}
-                                        line)
-                  valid-from (:xt/valid-from data)
-                  valid-to (:xt/valid-to data)
-                  enrich
-                  (cond
-                    (and valid-from valid-to)
-                    #(xt/during % valid-from valid-to)
-                    valid-from
-                    #(xt/starting-from % valid-from)
-                    valid-to
-                    #(xt/until % valid-to)
-                    :else
-                    identity)]
-              (-> (xt/put table-name data)
-                  enrich))))))))
+        (xt/submit-tx node (for [line line-batch
+                                 :let [{vf :xt/valid-from
+                                        vt :xt/valid-to
+                                        :as doc} (edn/read-string {:readers {'time/instant #(Instant/parse %)}}
+                                                                  line)]]
+                             [:put-docs {:into table-name, :valid-from vf, :valid-to vt}
+                              (dissoc doc :xt/valid-from :xt/valid-to)]))))))
 
 (def xt-node
   (let [node (xtn/start-node {})]
