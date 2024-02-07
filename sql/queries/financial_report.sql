d9a154997ec1f28bb60b7fc650b482669bb65ca4
Dan Stone <wotbrew@gmail.com>
2024-02-07 11:20:30 +0000

    improve financial_report.sql

--- sql/queries/financial_report.sql
+++ sql/queries/financial_report.sql
@@ -1,16 +1,21 @@
 -- :category "Custom"
 -- :desc "Monthly financial breakdown"
--- :col-order [:store_id, :month, :revenue]
-SELECT s.xt$id store_id,
-       m.mon AS month,
- -- sum of payments taken
-   (SELECT
-     COALESCE(FLOOR(SUM(p.amount)), 0.0)
-    FROM payment p
-    JOIN rental r ON r.xt$id = p.rental_id
-    JOIN inventory i ON i.xt$id = r.inventory_id
-    WHERE EXTRACT(MONTH FROM p.xt$valid_from) = m.mon
-    AND i.store_id = s.xt$id) revenue
-FROM store s, (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12)) AS m(mon)
-GROUP BY s.xt$id, m.mon
-ORDER BY s.xt$id, m.mon
+-- :col-order [:month, :rev23, :rev24]
+SELECT m.mon AS month,
+       (SELECT
+         COALESCE(FLOOR(SUM(p.amount)), 0.0)
+       FROM payment p
+              JOIN rental r ON r.xt$id = p.rental_id
+              JOIN inventory i ON i.xt$id = r.inventory_id
+       WHERE EXTRACT(MONTH FROM p.payment_date) = m.mon
+         AND EXTRACT(YEAR FROM p.payment_date) = 2023) rev23,
+       (SELECT
+         COALESCE(FLOOR(SUM(p.amount)), 0.0)
+       FROM payment p
+              JOIN rental r ON r.xt$id = p.rental_id
+              JOIN inventory i ON i.xt$id = r.inventory_id
+       WHERE EXTRACT(MONTH FROM p.payment_date) = m.mon
+         AND EXTRACT(YEAR FROM p.payment_date) = 2024) rev24
+FROM (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12)) AS m(mon)
+GROUP BY m.mon
+ORDER BY m.mon
