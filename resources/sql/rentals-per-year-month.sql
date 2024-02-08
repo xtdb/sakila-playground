f92c128566511a61f53c61a7018325e3ca853290
Yuriy Zaytsev <yurkazaytsev@gmail.com>
2024-02-08 18:08:08 +0000

    replace FOR condition with WHERE condition

--- resources/sql/rentals-per-year-month.sql
+++ resources/sql/rentals-per-year-month.sql
@@ -1,7 +1,10 @@
 WITH rentals_ym AS (
     SELECT EXTRACT (YEAR FROM rental.xt$valid_from) as year,
            EXTRACT (MONTH FROM rental.xt$valid_from) as month
-    FROM rental FOR VALID_TIME FROM ? TO ?)
+    FROM rental
+    WHERE rental.xt$valid_from >= COALESCE(?, rental.xt$valid_from) 
+          AND 
+          rental.xt$valid_from<=COALESCE(?, rental.xt$valid_from))
 SELECT A.year, A.month, count(*) as rented
 FROM rentals_ym as A
 GROUP BY A.year, A.month
--- resources/sql/top-rented-films.sql
+++ resources/sql/top-rented-films.sql
@@ -1,8 +1,11 @@
 WITH film_rented AS 
     (SELECT inventory.film_id, 
             count(*) as count_rented
-    FROM rental FOR VALID_TIME FROM ? TO ?
+    FROM rental 
     LEFT JOIN inventory ON rental.inventory_id = inventory.xt$id
+    WHERE rental.xt$valid_from >= COALESCE(?, rental.xt$valid_from) 
+        AND 
+        rental.xt$valid_from<=COALESCE(?, rental.xt$valid_from)
     GROUP BY inventory.film_id
     ORDER BY count_rented DESC
     LIMIT 10)
