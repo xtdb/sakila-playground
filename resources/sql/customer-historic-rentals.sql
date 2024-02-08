19395a89ac68634f874b2d211e1d7bf38b70a536
Dan Stone <wotbrew@gmail.com>
2024-02-08 15:21:27 +0000

    Fix errors in customer-historic-rentals.sql

--- resources/sql/customer-historic-rentals.sql
+++ resources/sql/customer-historic-rentals.sql
@@ -1,11 +1,11 @@
 SELECT rental.xt$id as id,
        film.title,
-       rental.rental_id, rental.customer_id, rental.inventory_id,
+       rental.customer_id, rental.inventory_id,
        rental.rental_date as rental_date,
        rental.return_date as return_date
 FROM rental
-LEFT JOIN customer ON (rental.customer_id = rental.xt$id)
-LEFT JOIN inventory ON (rental.inventory_id = inventory.xt$id)
-LEFT JOIN film ON (inventory.film_id = film.xt$id)
+JOIN customer ON rental.customer_id = customer.xt$id
+JOIN inventory ON rental.inventory_id = inventory.xt$id
+JOIN film ON inventory.film_id = film.xt$id
 WHERE rental.customer_id = ? AND rental.return_date IS NOT NULL
 ORDER BY rental.rental_date desc
--- sql/queries/customer_historic_rentals.sql
+++ sql/queries/customer_historic_rentals.sql
@@ -4,13 +4,12 @@
 -- :param-order [:id]
 SELECT rental.xt$id as id,
        film.title,
-       rental.rental_id, rental.customer_id, rental.inventory_id,
-       rental.rental_date,
-       rental.return_date
+       rental.customer_id, rental.inventory_id,
+       rental.rental_date as rental_date,
+       rental.return_date as return_date
 FROM rental
-LEFT JOIN customer ON (rental.customer_id = rental.xt$id)
-LEFT JOIN inventory ON (rental.inventory_id = inventory.xt$id)
-LEFT JOIN film ON (inventory.film_id = film.xt$id)
-WHERE rental.customer_id = ?
-AND rental.return_date IS NOT NULL
-ORDER BY rental.return_date desc
+       JOIN customer ON rental.customer_id = customer.xt$id
+       JOIN inventory ON rental.inventory_id = inventory.xt$id
+       JOIN film ON inventory.film_id = film.xt$id
+WHERE rental.customer_id = ? AND rental.return_date IS NOT NULL
+ORDER BY rental.rental_date desc
