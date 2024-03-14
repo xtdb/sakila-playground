37e7a78cbc04d45c09f843ce95ec7fede82fea5e
Jon Pither <jon@juxt.pro>
2024-03-14 20:53:20 +0000

    Trimming out old content

--- dev/nb/tutorial_part_3.clj
+++ dev/nb/tutorial_part_3.clj
@@ -72,9 +72,9 @@
 
 ;; How does this work with an immutable database?! Let's find out together.
 
-;; Let's pretend the first version of a product is inserted on 2024-01-01.
+;; Let's pretend the day today is 2024-01-01, and we insert a product:
 
-^{::clerk/no-cache true ::clerk/visibility {:code :hide :result :show}}
+^{::clerk/no-cache true ::clerk/visibility {:code :hide :result :hide}}
 (set-time #inst "2024-01-01")
 
 ^{::clerk/no-cache true}
@@ -87,15 +87,18 @@
 ^{::clerk/no-cache true}
 (q "SELECT * FROM product FOR VALID_TIME AS OF DATE '2024-01-02'")
 
-;; Now, let's query against the past - 2023
-;; We should NOT see any data, because the product was inserted into the database on 2024-01-01:
+;; Now, let's query against the past, in `2023`
+
+;; We should NOT see any data, because the product was inserted into the database on `2024-01-01`:
 
 ^{::clerk/no-cache true}
 (q "SELECT * FROM product FOR VALID_TIME AS OF DATE '2023-01-01'")
 
-;; But let's say, we want to insert some historical data into our database - 2022.
-;; This could an import from another system, into our golden store
-;; We can do this in XT but setting the xt$valid_from column
+;; But let's say, we want to insert some historical data into our database, all the way back in `2022`.
+
+;; This could an import from another system, into our XTDB golden store.
+
+;; We achieve this in XT by setting the xt$valid_from column
 
 ^{::clerk/no-cache true}
 (e "INSERT INTO product (xt$id, name, price, xt$valid_from)"
@@ -116,68 +119,6 @@
 
 ^{::clerk/no-cache true}
 (q "SELECT * FROM product FOR VALID_TIME AS OF DATE '2020-01-01'")
-
-
-;; NEED TO INSERT WITH VALID_TIME
-
-;; THEN SHOW WITH SYSTEM_TIME, CAN GET ORIGINAL DATA BACK
-;; SYSTEM_TIME shadows VALID_TIME, but is completely immutable
-;; This is the concept of bitemporality
-
-;; A month later, we decide to update the price of the product.
-
-^{::clerk/no-cache true ::clerk/visibility {:code :hide :result :show}}
-(set-time #inst "2024-02-01")
-
-^{::clerk/no-cache true}
-(e "UPDATE product"
-   "SET price = 360"
-   "WHERE product.name = 'Pendleton Electric Bicycle'")
-
-;; Let's check the new price:
-
-^{::clerk/no-cache true}
-(q "SELECT * FROM product")
-
-;; A month later, with part costs still increasing, we increase the price again.
-
-^{::clerk/no-cache true ::clerk/visibility {:code :hide :result :show}}
-(set-time #inst "2024-03-01")
-
-^{::clerk/no-cache true}
-(e "UPDATE product"
-   "SET price = 400"
-   "WHERE product.name = 'Pendleton Electric Bicycle'")
-
-^{::clerk/no-cache true}
-(q "SELECT * FROM product")
-
-;; Our CFO is running a report about January sales and needs to know
-;; the price of every product as of 2024-01-15.
-
-^{::clerk/no-cache true}
-(q "SELECT * FROM product FOR VALID_TIME AS OF DATE '2024-01-15'")
-
-;; Now the CFO wants to how the prices have increased over Q1.
-
-^{::clerk/no-cache true}
-(q "SELECT product.*, product.xt$valid_from, product.xt$valid_to"
-   "FROM product"
-   "FOR ALL VALID_TIME"
-   "ORDER BY product.xt$valid_from")
-
-;; When did the bicycle price first exceed $350 ?
-
-^{::clerk/no-cache true}
-(q "SELECT product.*, product.xt$valid_from, product.xt$valid_to"
-   "FROM product"
-   "FOR ALL VALID_TIME"
-   "WHERE product.price > 350"
-   "ORDER BY product.xt$valid_from"
-   "LIMIT 1")
-
-;; Yes, it was the price change of 2024-02-01 where the bicycle's
-;; price exceeded $350.
 
 ^{::clerk/visibility {:code :hide, :result :hide}}
 (comment
