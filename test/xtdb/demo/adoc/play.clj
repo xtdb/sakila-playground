64a993b58a04209fe6210452c30f4dc017b09464
Malcolm Sparks <mal@juxt.pro>
2024-03-19 11:21:43 +0000

    Add adoc dependency and a quick extension registration

---
+++ test/xtdb/demo/adoc/play.clj
@@ -0,0 +1,35 @@
+;; Copyright © 2024, JUXT LTD.
+
+(ns xtdb.demo.adoc.play
+  (:require
+   [clojure.test :as t]
+   [clojure.reflect :as reflect])
+  (:import
+   (org.asciidoctor.extension ExtensionGroup BlockMacroProcessor)
+   (org.asciidoctor Asciidoctor Options)))
+
+
+
+(let [adoc (Asciidoctor$Factory/create)
+      extension-group
+      (.. adoc
+          (createGroup "my-extensions")
+          (blockMacro
+           (proxy [BlockMacroProcessor] []
+             (process [parent target attributes]
+               (proxy-super createBlock parent "pass" "working!"))
+             (getName [] "sql")))
+          register)
+      options
+      (.. (Options/builder)
+          (build))]
+
+  (.convert adoc
+            "Hello World!
+
+sql::mygithubaccount/8810011364687d7bec2c[]
+"
+            options
+            )
+
+  )
