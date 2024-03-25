388a832f8d3a53cf7bdc62f9b015b67d15382764
Malcolm Sparks <mal@juxt.pro>
2024-03-25 15:10:13 +0000

    checkpoint

--- src/xtdb/demo/adoc/resources.clj
+++ src/xtdb/demo/adoc/resources.clj
@@ -6,30 +6,150 @@
    [xtdb.demo.web.resource :refer [map->Resource html-resource html-templated-resource]]
    [xtdb.api :as xt]
    [xtdb.node :as xtn]
-   [clojure.java.io :as io])
+   [clojure.java.io :as io]
+   [clojure.reflect :as reflect]
+   [clojure.string :as str])
   (:import
-   (org.asciidoctor.extension ExtensionGroup BlockMacroProcessor)
+   (org.asciidoctor.extension ExtensionGroup BlockMacroProcessor BlockProcessor)
    (org.asciidoctor Asciidoctor Options)))
+
+;; See https://docs.asciidoctor.org/asciidoctorj/latest/extensions/ast-introduction/
 
 ;; http://localhost:3010/adoc/tutorial_part_1
 
-(defonce raw-adoc (org.asciidoctor.Asciidoctor$Factory/create))
+
+(def raw-adoc (org.asciidoctor.Asciidoctor$Factory/create))
+
+(defn parse-date [s]
+  (java.util.Date/from
+   (.toInstant (.atZone (.atStartOfDay (java.time.LocalDate/parse s)) (java.time.ZoneId/of "Z")))))
 
 (defn extend-adoc [adoc xt-node]
-  (doto adoc
-    (.. (createGroup "my-extensions")
-        (blockMacro
-         (proxy [BlockMacroProcessor] []
-           (process [parent target attributes]
-             (case target
-               "set-simulation-time"
-               (proxy-super createBlock parent "pass" (format "Setting simulation time to %s" (pr-str attributes)))
-               (proxy-super
-                createBlock parent "pass"
-                (format "Unknown block macro processor target: '%s' %s" target (type target)))))
+  (let [current-time (atom (java.util.Date.))]
+    (doto adoc
+      (..
+       (createGroup "my-extensions")
 
-           (getName [] "xt")))
-        register)))
+       (blockMacro
+        (proxy [BlockMacroProcessor] ["xt"]
+          (process [parent target attributes]
+            (case target
+              "set-simulation-time"
+              (let [date (parse-date (get attributes "inst"))]
+                (reset! current-time date)
+                (proxy-super createBlock parent "paragraph" (format "Setting simulation time: `%s`" date)))
+              (proxy-super
+               createBlock parent "listing"
+               (format "Unknown block macro processor target: '%s' %s" target (type target)))))))
+
+       (block
+        (proxy [BlockProcessor] ["xtsubmit"]
+          (process [parent reader attributes]
+            (let [sql (.read reader)]
+              (xt/submit-tx xt-node [[:sql sql]] {:system-time @current-time})
+              (proxy-super
+               createBlock parent "listing"
+               sql
+               )))))
+
+       (block
+        (proxy [BlockProcessor] ["xtquery"]
+          (process [parent reader attributes]
+            (let [sql (.read reader)
+                  rs (xt/q xt-node sql {:current-time @current-time
+                                        :key-fn :snake-case-string})]
+
+
+              #_(.load adoc
+                       (str/join "\n" ["----" sql "----" ".Results" "----"
+                                       (str/join "\n" (for [row rs] (pr-str row)))
+                                       "----"
+                                       (type parent)]
+
+
+
+                                 )
+                       (.. (Options/builder)
+                           (build)))
+              (let [structure
+                    [:section {:title "SQL listing"}
+                     [:listing {} "foo"]]
+
+                    #_generator
+                    #_(fn gen [root parent [k atts & content]]
+                      (let [block
+                            (case k
+                              :section (let [block
+                                             (cond-> (proxy-super
+                                                      createSection root
+                                                      false {})
+                                               (:title atts) (.setTitle (:title atts))
+
+                                               )]
+                                         block
+                                         )
+                              :listing (proxy-super
+                                        createBlock root "listing"
+                                        "sql"
+                                        ))]
+                        (println "content is" (pr-str content))
+                        (doseq [child content
+                                :when (vector? child)]
+                          (println "appending to parent" parent "child" child)
+                          (.append root (gen root parent child))
+                          )
+                        ))]
+
+                #_(generator parent parent structure)
+
+                (let [section
+                      (proxy-super
+                       createSection parent
+                       false {})
+                      _ (.setTitle section "SQL listing")
+
+                      listing
+                      (proxy-super
+                       createBlock section "listing"
+                       sql
+                       )
+                      _ (.setTitle listing "listing-title")
+                      _ (.append section listing)
+
+                      results-para
+                      (proxy-super
+                       createBlock section "paragraph"
+                       "and the results are:")
+                      _ (.append section results-para)
+
+                      table
+                      (proxy-super createTable section)
+
+                      column (proxy-super createTableColumn table 0 {})
+                      _ (.add (.getColumns table) column)
+
+                      row
+                      (proxy-super createTableRow table)
+
+                      _ (println (.getBody table) (type (.getBody table)))
+                      _ (println (.isEmpty (.getBody table)))
+
+                      ;;_ (println (.getBody table))
+                      _ (println "row" row (type row))
+                      _ (.clear (.getBody table))
+
+                      ;;_ (.set (.getBody table) 0 row)
+
+                      ;;_ (.append results-table row)
+                      ;;_ (.append section table)
+                      ]
+
+                  section))))))
+
+       register))))
+
+;;(reflect/reflect org.asciidoctor.jruby.ast.impl.TableImpl$RowList)
+;;(reflect/reflect org.asciidoctor.jruby.ast.impl.RowImpl)
 
 #_(.. (Options/builder)
       ;;(backend "docbook")
